2955d01e14ec7ecb054a1f46bc2656f25ffda32f
Antonio Miguel <albino77miguel@gmail.com>
2022-12-05 08:50:44 +0100

    Meu primeiro App React Native

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Helloworld</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +13,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center'
+  
   },
 });
